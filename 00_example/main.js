7bf7f9b70a10e548c4281d08c92d08decacbacf4
SUZIN <suuz.hhh@gamil.com>
2021-03-01 14:00:04 +0900

    add

--- 00_example/main.js
+++ 00_example/main.js
@@ -1,29 +1,49 @@
+var http = require("http");
+var fs = require("fs");
+var url = require("url"); //url 모듈 사용
 
-var http = require('http');
-var fs = require('fs');
-var url = require('url');//url 모듈 사용
-
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url,true).query;
-    console.log('queryDate:'+queryData.id);
-    if(_url == '/'){
-      _url = '/index.html';
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    response.end(queryData.id);
- 
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query; // ? id = 의 값
+  // console.log('queryDate:'+queryData.id); // ? id = 의 값
+  var title = queryData.id;
+  if (_url == "/") {
+    title = 'welcome';
+  }
+  if (_url == "/favicon.ico") {
+    return response.writeHead(404);
+  }
+  response.writeHead(200);
+  var template = `
+    <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ol>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ol>
+      <h2>${title}</h2>
+      <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+      <img src="coding.jpg" width="100%">
+      </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
+      </p>
+    </body>
+    </html>`;
+  response.end(template);
 });
 app.listen(3000);
-// if port number => 80 생략가능 기본값 
-//                
+// if port number => 80 생략가능 기본값
+//
 // http://tmp:3000/main?html&page=12
-//  
+//
 // protocol , host(domain) , port ,path ,query srting
-// query string strat : ?   var & var = value 
+// query string strat : ?   var & var = value
 // path 컴퓨터 안에있는 어떤디렉토리의 어떤파일인지 가르킴
 //
 //
